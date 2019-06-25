2cc1918d4a5a42a5c38b61efad812a5b401a37dd
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-06-25 11:10:40 +0530

    Create set13.py

---
+++ set13.py
@@ -0,0 +1,7 @@
+var=input()
+if(var=='A' or var=='a' or var=='E' or var=='e' or var=='i' or var=='I' or var=='o' or var=='O' or var=='u' or var=='U'):
+  print("Vowel")
+elif(var!='A' or var!='a' or var!='e' or var!='E' or var!='i' or var!='o' or var!='O' or var!='u' or var!='U' or var!='I'):
+  print("Consonant")
+else:
+  print("invalid")
