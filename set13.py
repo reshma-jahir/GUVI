a98e29be57b76ee06a4ca5bfe527d431c4b1463a
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-06-25 11:25:00 +0530

    Update set13.py

--- set13.py
+++ set13.py
@@ -1,4 +1,4 @@
-var=input()
+var=raw_input()
 if(var=='A' or var=='a' or var=='E' or var=='e' or var=='i' or var=='I' or var=='o' or var=='O' or var=='u' or var=='U'):
   print("Vowel")
 elif(var!='A' or var!='a' or var!='e' or var!='E' or var!='i' or var!='o' or var!='O' or var!='u' or var!='U' or var!='I'):
