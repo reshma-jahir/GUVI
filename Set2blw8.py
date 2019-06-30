086126ffb4e8da82e15729d85a126ce033910609
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-06-30 21:02:38 +0530

    Update and rename Set2in8.py to Set2blw8.py

---
+++ Set2blw8.py
@@ -0,0 +1,10 @@
+ex,oi=map(int,input().split())
+for i in range(ex+1,oi):
+    finally=i
+    fine=0
+    while(i>0):
+        dad=i%10
+        fine=fine+(dad**3)
+        i=i//10
+        if(fine==finally):
+            print(fine, end=" ")
