66865c469449a473b799bbe8ab6dfd94ddee585c
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-05 02:34:54 -0700

    Update and rename set56.py to setla56.py

--- set56.py
+++
@@ -1,6 +0,0 @@
-rr=input()
-for i in range(0,len(rr)):
- if(rr[i].isalpha() and rr[i].isdigit()):
-  print("No")
-else:
- print("Yes")
---
+++ setla56.py
@@ -0,0 +1,6 @@
+rrt=input()
+for i in range(0,len(rrt)):
+ if(rrt[i].isalpha() and rrt[i].isdigit()):
+  print("No")
+else:
+ print("Yes")
