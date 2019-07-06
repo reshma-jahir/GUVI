3f59d578d96e8e7a1fec2692d412090541b133a9
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-06 22:38:40 +0530

    Update set85.py

--- set85.py
+++ set85.py
@@ -1,4 +1,8 @@
-import math
-xy=float(input())
-xy=math.ceil(xy)
-print(xy)
+strgs=list(input())
+if len(strgs)%2==0:
+    strgs[int(len(strgs)/2)] ='*'
+    strgs[int(len(strgs)/2)-1]='*'
+else:
+    strgs[int(len(strgs)/2)] ='*'
+for i in range(0,len(strgs)):
+    print(strgs[i],end='')
