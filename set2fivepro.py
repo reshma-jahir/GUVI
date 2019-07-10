5e65a7a944becc8bdc4111a5207ddf5d771747e4
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-10 17:21:41 +0530

    Create set2fivepro.py

---
+++ set2fivepro.py
@@ -0,0 +1,10 @@
+lawt=input()
+yawt=map(int,input().split())
+pawt=[]
+for g in yawt:
+    enum=bin(g)
+    pawt.append(enum)
+fraud=sorted(paw)
+fraud.reverse()
+for h in fraud:
+    print(int(h,2))
