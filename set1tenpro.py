0865c7b2d00fb845e87ee548d8d01b96b9d837a1
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-10 17:03:11 +0530

    Create set1tenpro.py

---
+++ set1tenpro.py
@@ -0,0 +1,8 @@
+chat=int(input())
+chot=[int(o) for o in input().split(" ")]
+chet=0
+for p in range(chat):
+      for g in range(p):
+           if(chot[g]<chot[p]):
+                chet+=chot[g]
+print(chet)
