8230c63ead199af364e9c93ef4cba69ad829567d
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-10 16:27:39 +0530

    Update and rename set1fourpro.py to proset1four.py

--- proset1four.py
+++ proset1four.py
@@ -4,8 +4,8 @@ if len(sab)>len(sar):
 	sab,sar=sar,sab
 p=0
 while p<len(sab):
-	  yas+=(ord(sar[p])-ord(sab[p]))
+	  yast+=(ord(sar[p])-ord(sab[p]))
 	  p+=1
 for p in range(p,len(sar)):
-	  yas+=ord(sar[p])-ord('a')+1
+	  yast+=ord(sar[p])-ord('a')+1
 print(yast)
