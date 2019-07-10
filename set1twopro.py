be942b40f7efe4292f36c0cbc283a13e12ca75f0
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-10 15:48:13 +0530

    Create set1twopro.py

---
+++ set1twopro.py
@@ -0,0 +1,13 @@
+num,kum=input().strip().split(" ")
+kit=int(kit)
+i=0;
+while i<len(num)-1 and kit:
+	if(num[i]>num[i+1]):
+		kit-=1
+		num=num[:i]+num[i+1:]
+		if(i!=0):
+			i-=1
+	else:
+		i+=1
+rey=num[:len(num)-k]
+print(rey)
