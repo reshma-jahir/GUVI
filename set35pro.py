1b8bf14f9c5fe879db458558d454a6a7a4e44873
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-16 16:57:33 +0530

    Create set35pro.py

---
+++ set35pro.py
@@ -0,0 +1,13 @@
+number=input()
+level=list(map(int,input().split()))
+maximum=0
+i=0
+while(i<len(l)-1):
+    count=0
+    while(i<len(level)-1 and level[i]<level[i+1]):
+        count+=1
+        i+=1
+    if(count>maximum):
+        maximum=c
+    i+=1
+print(maximum+1)
