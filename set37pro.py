8be7526dfa8e1d1eedda1c5e151c393ac209b702
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-16 17:20:04 +0530

    Create set37pro.py

---
+++ set37pro.py
@@ -0,0 +1,17 @@
+Aa,Bb=map(int,input().split())
+Cc=list(map(int,input().split()))
+pr=list(map(int,input().split()))
+qr=[]
+ar=0
+for i in range(Aa):
+    x=pr[i]/C[i]
+    qr.append(x)
+while Bb>=0 and len(qr)>0:
+    mindex=qr.index(max(qr))
+    if Bb>=C[mindex]:
+        ar=ar+pr[mindex]
+        Bb=Bb-Cc[mindex]
+    Cc.pop(mindex)
+    pr.pop(mindex)
+    qr.pop(mindex)
+print(ar)
