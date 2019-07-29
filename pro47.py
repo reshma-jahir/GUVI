5a0bb7e42eba4d524b457e6e65edbf546dbb058a
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-29 18:28:40 +0530

    Create pro47.py

---
+++ pro47.py
@@ -0,0 +1,19 @@
+nt11=int(input())
+lt=list(map(int,input().split()))
+ct=0
+it=0
+while(it<len(lt)):
+    tt=lt[it]
+    if(it==0):
+        if(len(lt)==1):
+            ct=1 
+            break
+    elif(it==len(lt)-1):
+        ct=ct
+    else:
+        if(tt>lt[it+1] and tt>lt[it-1]):
+            ct=ct+1
+        elif(tt<lt[it-1] and tt<lt[it+1]):
+            ct=ct+1
+    it=it+1
+print(ct)
