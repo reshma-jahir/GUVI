65d6f1bec5868fa5c305d7c9d02b07e555e25233
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-30 07:40:34 +0530

    Update pro61.py

--- pro61.py
+++ pro61.py
@@ -1,11 +1,23 @@
-pin = int(input())
-a,val = 3,3
-while pin > 0:
-    if a == 0:
-        val*=2
-        a = val
-    if pin==1:
-        print(a)
-        break
-    pin -= 1
-    a -= 1
+def charm(l):
+        bhh=1
+        
+        for xt in range(0,len(l)-1):
+                if l[xt]!=l[xt+1]:
+                        bhh=bhh+1
+                else:
+                    break
+        return bhh
+num1=int(input())
+hiit=list(map(int,input().split()))
+
+for xt in range(0,len(hiit)):
+        if hiit[xt]>0:
+                hiit[xt]=1
+        else:
+             hiit[xt]=0
+A1=""
+
+for xt in range(0,len(hiit)):
+        B1=hiit[xt::]
+        A1=A1+str(charm(B1))+" "
+print(A1.strip())
