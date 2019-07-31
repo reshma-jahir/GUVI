5cb0ca20e43381aa11227b996c654c98cea2a341
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-31 07:07:33 +0530

    Update and rename pro76.py to pro76inpro.py

--- pro76inpro.py
+++ pro76inpro.py
@@ -1,16 +1,16 @@
 pp, Qp, Fp, Kd = map(int, input().split())
 cnt = 0
-Dv = Qp-Fp
-if (Dv >= 0):
+Dvd = Qp-Fp
+if (Dvd >= 0):
     S1 = (pp-Fp)*2
     for X in range (Kd):
         if (X == Kd-1):
              S1 =S1/ 2
-        if (Dv < S1):
-            Dv= Qp
+        if (Dvd < S1):
+            Dvd= Qp
             cnt += 1
-        Dv = Dv - S1
-        if (Dv < 0):
+        Dvd = Dvd - S1
+        if (Dvd < 0):
             cnt = -1
             break
         S1 = 2*pp - S1
