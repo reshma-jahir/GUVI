8a225a7f0313f9849de0ea4dff0832d67d7ed572
reshma-jahir <51981013+reshma-jahir@users.noreply.github.com>
2019-07-31 06:58:53 +0530

    Update pro72.py

--- pro72.py
+++ pro72.py
@@ -1,17 +1,17 @@
 import sys
-def palindrome(dk11):
-    if len(dk11) == 1:
+def palindrome(dk1):
+    if len(dk1) == 1:
         return False
-    if dk11 == dk11[::-1]:
+    if dk1 == dk1[::-1]:
         return True
     return False
 dk1 = input()
-n1 = len(dk11)
-for i in range(n1-1, 0, -1):
-    for j in range(0, n1-i):
+num = len(dk1)
+for i in range(num-1, 0, -1):
+    for j in range(0, num-i):
         i1 = j
         i2 = j+i+1
-        d2 = dk11[i1:i2]
+        d2 = dk1[i1:i2]
         if palindrome(d2):
-            print(n1-len(d2))
+            print(num-len(d2))
             sys.exit()
